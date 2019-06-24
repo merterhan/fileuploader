5b276e9ed63f078cf5e8d10163d0d996e1f24a1d
Mert Çağrıberk Erhan <mertcagriberkerhan@gmail.com>
2019-06-24 14:00:53 +0300

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            HttpFileCollectionBase files = Request.Files;
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                HttpPostedFileBase file = files[i];
+                string path = Server.MapPath("~") + "Uploads\\" + file.FileName;
+                file.SaveAs(path);
+            }            
+
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
